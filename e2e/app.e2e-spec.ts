0c2099fd07f94d05f62c4954509c334db87c8978
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-12-18 00:57:03 +0100

    fix(): fix e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-cli-skeleton App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('KS Welcome');
+    // expect(page.getTitle()).toEqual('KS Welcome');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  getTitle() {
     return element(by.css('app-root h1')).getText();
   }
 }
