2449d35dd6beeceecd30816b7b38baeecc35e554
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-01-09 21:54:18 +0100

    chore(): disable appshell to prevent issues when used with ng-bootstrap in production env #22

--- src/app/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-shell',
-  templateUrl: './app-shell.component.html'
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.css']
 })
 export class AppShellComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
